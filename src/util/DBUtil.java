267c6f30f348bfc21018d41401e4d3ea84cd335e
shinichirou.yokota <shinichi3427@gmail.com>
2020-05-22 23:49:41 +0900

    Add database files

---
+++ src/util/DBUtil.java
@@ -0,0 +1,21 @@
+package util;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class DBUtil {
+    private static final String PERSISTENS_UNIT_NAME = "kadai-tasklist";
+    private static EntityManagerFactory emf;
+
+    public static EntityManager createEntityManager() {
+        return _getEntityManagerFactory().createEntityManager();
+    }
+
+    private static EntityManagerFactory _getEntityManagerFactory() {
+        if (emf == null) {
+            emf = Persistence.createEntityManagerFactory(PERSISTENS_UNIT_NAME);
+        }
+        return emf;
+    }
+}
